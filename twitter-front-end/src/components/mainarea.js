0665f6f1408e06fe25889b7ddf295160988a9d84
yschnell <yl.schnell@gmail.com>
2020-12-16 14:12:48 +0100

    the Main area part of thev tweeter app

---
+++ twitter-front-end/src/components/mainarea.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+export default function Mainarea() {
+    return (
+        <div className="Main_Area">
+        Main_Area
+        </div>
+    )
+}
---
+++ twitter-front-end/src/components/sidebaruserinfo.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const sidebaruserinfo = props => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+sidebaruserinfo.propTypes = {
+
+}
+
+export default sidebaruserinfo
+
