6b4f09b80fbe9e79c9236bd919aa1a483e4fbce5
yschnell <yl.schnell@gmail.com>
2020-12-16 10:32:38 +0100

    yl changes twitter app

---
+++ twitter-clone/src/components/mainpage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const mainpage = props => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+mainpage.propTypes = {
+
+}
+
+export default mainpage
